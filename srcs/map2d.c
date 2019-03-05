ecb664b7e74bafcd8b1b3fb85c7cf2708ef2a03a
Cosmin marius BILGA <cbilga@e1r4p5.42.fr>
2019-03-05 19:12:29 +0100

    norme done

--- srcs/map2d.c
+++ srcs/map2d.c
@@ -6,7 +6,7 @@
 /*   By: cbilga <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 10:59:36 by cbilga            #+#    #+#             */
-/*   Updated: 2019/03/05 13:11:17 by lomasse          ###   ########.fr       */
+/*   Updated: 2019/03/05 18:46:56 by cbilga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,68 +29,78 @@ static void	draw_ray(t_acz *az)
 	}
 }
 
+static void	draw_obstacle3(t_acz *az, SDL_Rect *p, SDL_Rect *block)
+{
+	az->map->map[p->y][p->x] == 0 ? SDL_SetRenderDrawColor(az->main->rend,
+						150, 150, 150, 0) : 0;
+	az->map->map[p->y][p->x] == 1 ? SDL_SetRenderDrawColor(az->main->rend,
+						0, 0, 0, 0) : 0;
+	az->map->map[p->y][p->x] == 2 ? SDL_SetRenderDrawColor(az->main->rend,
+						150, 0, 150, 0) : 0;
+	az->map->map[p->y][p->x] == 3 ? SDL_SetRenderDrawColor(az->main->rend,
+						250, 0, 0, 0) : 0;
+	az->map->map[p->y][p->x] == 4 ? SDL_SetRenderDrawColor(az->main->rend,
+						250, 180, 0, 0) : 0;
+	az->map->map[p->y][p->x] == 5 ? SDL_SetRenderDrawColor(az->main->rend,
+						0, 250, 0, 0) : 0;
+	az->map->map[p->y][p->x] == 6 ? SDL_SetRenderDrawColor(az->main->rend,
+						0, 0, 255, 0) : 0;
+	az->map->map[p->y][p->x] == 7 ? SDL_SetRenderDrawColor(az->main->rend,
+						250, 200, 0, 0) : 0;
+	az->map->map[p->y][p->x] == 8 ? SDL_SetRenderDrawColor(az->main->rend,
+						200, 200, 255, 0) : 0;
+	az->map->map[p->y][p->x] == 9 ? SDL_SetRenderDrawColor(az->main->rend,
+						200, 250, 100, 0) : 0;
+	az->map->map[p->y][p->x] == ENEMY ? SDL_SetRenderDrawColor(az->main->rend,
+						100, 0, 0, 0) : 0;
+	block->x = p->w * SBLOCK;
+	block->y = p->h * SBLOCK;
+	SDL_RenderFillRect(az->main->rend, block);
+}
+
+static int	draw_obstacle2(t_acz *az, SDL_Rect *p, SDL_Rect *block)
+{
+	if (p->y >= 60)
+		return (1);
+	p->x = (int)az->map->persox - 6;
+	p->w = 0;
+	while (p->x < (int)az->map->persox + 10 || p->x <= 60)
+	{
+		while (p->x < 0)
+		{
+			p->x++;
+			p->w++;
+		}
+		if (p->x >= 60 || p->y >= 60 || p->y < 0)
+			break ;
+		draw_obstacle3(az, p, block);
+		SDL_RenderFillRect(az->main->rend, block);
+		p->x++;
+		p->w++;
+	}
+	p->y++;
+	p->h++;
+	return (0);
+}
+
 static void	draw_obstacle(t_acz *az)
 {
-	int			x;
-	int			y;
-	int			x1;
-	int			y1;
+	SDL_Rect	p;
 	SDL_Rect	block;
 
 	block.w = SBLOCK;
 	block.h = SBLOCK;
-	y = (int)az->map->persoy - 5;
-	y1 = 0;
-	while (y < (int)az->map->persoy + 10 || y <= 60)
+	p.y = (int)az->map->persoy - 5;
+	p.h = 0;
+	while (p.y < (int)az->map->persoy + 10 || p.y <= 60)
 	{
-		while (y < 0)
+		while (p.y < 0)
 		{
-			y++;
-			y1++;
+			p.y++;
+			p.h++;
 		}
-		if (y >= 60)
-			break;
-		x = (int)az->map->persox - 6;
-		x1 = 0;
-		while (x < (int)az->map->persox + 10 || x <= 60)
-		{
-			while (x < 0)
-			{
-				x++;
-				x1++;
-			}
-			if (x >= 60 || y >= 60 || y < 0)
-				break ;
-			az->map->map[y][x] == 0 ? SDL_SetRenderDrawColor(az->main->rend,
-					150, 150, 150, 0) : 0;
-			az->map->map[y][x] == 1 ? SDL_SetRenderDrawColor(az->main->rend,
-					0, 0, 0, 0) : 0;
-			az->map->map[y][x] == 2 ? SDL_SetRenderDrawColor(az->main->rend,
-					150, 0, 150, 0) : 0;
-			az->map->map[y][x] == 3 ? SDL_SetRenderDrawColor(az->main->rend,
-					250, 0, 0, 0) : 0;
-			az->map->map[y][x] == 4 ? SDL_SetRenderDrawColor(az->main->rend,
-					250, 180, 0, 0) : 0;
-			az->map->map[y][x] == 5 ? SDL_SetRenderDrawColor(az->main->rend,
-					0, 250, 0, 0) : 0;
-			az->map->map[y][x] == 6 ? SDL_SetRenderDrawColor(az->main->rend,
-					0, 0, 255, 0) : 0;
-			az->map->map[y][x] == 7 ? SDL_SetRenderDrawColor(az->main->rend,
-					250, 200, 0, 0) : 0;
-			az->map->map[y][x] == 8 ? SDL_SetRenderDrawColor(az->main->rend,
-					200, 200, 255, 0) : 0;
-			az->map->map[y][x] == 9 ? SDL_SetRenderDrawColor(az->main->rend,
-					200, 250, 100, 0) : 0;
-			az->map->map[y][x] == ENEMY ? SDL_SetRenderDrawColor(az->main->rend,
-					100, 0, 0, 0) : 0;
-			block.x = x1 * SBLOCK;
-			block.y = y1 * SBLOCK;
-			SDL_RenderFillRect(az->main->rend, &block);
-			x++;
-			x1++;
-		}
-		y++;
-		y1++;
+		if (draw_obstacle2(az, &p, &block))
+			break ;
 	}
 }
 
--- srcs/move.c
+++ srcs/move.c
@@ -1,96 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   input.c                                            :+:      :+:    :+:   */
+/*   move.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lomasse <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: cbilga <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/08 15:25:07 by lomasse           #+#    #+#             */
-/*   Updated: 2019/03/05 11:04:25 by lomasse          ###   ########.fr       */
+/*   Created: 2019/03/05 18:53:27 by cbilga            #+#    #+#             */
+/*   Updated: 2019/03/05 19:09:19 by cbilga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/wolf3d.h"
-
-void		collision(t_acz *az)
-{
-	if (az->map->persox > 60)
-	{
-		az->map->persox = 0.5;
-		if (az->map->map[(int)az->map->persoy][(int)az->map->persox] == 1)
-			az->map->persox = 59.5;
-	}
-	if (az->map->persoy > 60)
-	{
-		az->map->persoy = 0.5;
-		if (az->map->map[(int)az->map->persoy][(int)az->map->persox] == 1)
-			az->map->persoy = 59.5;
-	}
-	if (az->map->persox <= 0)
-	{
-		az->map->persox = 59.5;
-		if (az->map->map[(int)az->map->persoy][(int)az->map->persox] == 1)
-			az->map->persox = 0.5;
-	}
-	if (az->map->persoy <= 0)
-	{
-		az->map->persoy = 59.5;
-		if (az->map->map[(int)az->map->persoy][(int)az->map->persox] == 1)
-			az->map->persoy = 0.5;
-	}
-	if (az->map->map[(int)az->map->persoy][(int)az->map->persox] == 3)
-	{
-		az->interface = 0;
-		ft_putstr("You win\n");
-	}
-	else if (az->map->map[(int)az->map->persoy][(int)az->map->persox] == 8)
-	{
-		az->inv->health < 100 ? az->inv->health = 100 : 0;
-		az->map->map[(int)az->map->persoy][(int)az->map->persox] = 0;
-	}
-	else if (az->map->map[(int)az->map->persoy][(int)az->map->persox] == 9)
-	{
-		az->inv->shield < 100 ? az->inv->shield = 100 : 0;
-		az->map->map[(int)az->map->persoy][(int)az->map->persox] = 0;
-	}
-	else if (az->map->map[(int)az->map->persoy][(int)az->map->persox] == 1)
-	{
-		az->map->persox -= az->map->lastmovx;
-		az->map->persoy -= az->map->lastmovy;
-	}
-	else if (az->map->map[(int)az->map->persoy][(int)az->map->persox] == 4)
-	{
-		az->inv->key += 1;
-		az->map->map[(int)az->map->persoy][(int)az->map->persox] = 0;
-	}
-	else if (az->map->map[(int)az->map->persoy][(int)az->map->persox] == 5)
-	{
-		if (az->inv->key > 0)
-		{
-			az->inv->key -= 1;
-			az->map->map[(int)az->map->persoy][(int)az->map->persox] = 0;
-		}
-		else
-		{
-			az->map->persox -= az->map->lastmovx;
-			az->map->persoy -= az->map->lastmovy;
-		}
-	}
-	if (az->map->map[(int)az->map->persoy][(int)az->map->persox] == 6 &&
-			az->map->orange[0] != 0)
-	{
-		az->map->persoy = az->map->orange[1] / SBLOCK;
-		az->map->persox = az->map->orange[2] / SBLOCK;
-		changeray(az, 6);
-	}
-	else if (az->map->map[(int)az->map->persoy][(int)az->map->persox] == 7 &&
-			az->map->blue[0] != 0)
-	{
-		az->map->persoy = az->map->blue[1] / SBLOCK;
-		az->map->persox = az->map->blue[2] / SBLOCK;
-		changeray(az, 7);
-	}
-}
 
 void		rotate_perso(t_acz *az)
 {
